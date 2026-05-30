e183e2174f7e20950816e6422a5459b8a6087a82
Breno <Breno@Breno.com>
2026-05-30 11:00:34 -0300

    Cadastro e Login funcionando

--- Outro/Controllers/HomeController.cs
+++ Outro/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Outro.Models;
 using TCC.Data;
+using MySql.Data.MySqlClient;
 
 namespace Outro.Controllers
 {
@@ -37,9 +38,90 @@ namespace Outro.Controllers
             return View();
         }
 
-        public ActionResult Cadastro()
+        [HttpPost]
+        public IActionResult Login(string email, string senha)
+        {
+            Conexao conexao = new Conexao();
+
+            if (conexao.AbrirConexao())
+            {
+                string sql =
+                    "SELECT * FROM usuarios " +
+                    "WHERE email = @email AND senha = @senha";
+
+                MySqlCommand cmd =
+                    new MySqlCommand(sql, conexao.conectar);
+
+                cmd.Parameters.AddWithValue("@email", email);
+                cmd.Parameters.AddWithValue("@senha", senha);
+
+                MySqlDataReader reader =
+                    cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    reader.Close();
+                    conexao.FecharConexao();
+
+                    return RedirectToAction("Dashboard");
+                }
+
+                reader.Close();
+                conexao.FecharConexao();
+            }
+
+            ViewBag.Erro = "Email ou senha inválidos.";
+
+            return View();
+        }
+
+        // Abre a pagina
+        [HttpGet]
+        public IActionResult Cadastro()
         {
             return View();
+        }
+        // Recebe os dados do formulario
+        [HttpPost]
+        public IActionResult Cadastro(Usuario usuario)
+        {
+            Conexao conexao = new Conexao();
+
+            if (conexao.AbrirConexao())
+            {
+                string sql =
+                    "INSERT INTO usuarios(nome,serie,telefone,email,senha) " +
+                    "VALUES(@nome,@serie,@telefone,@email,@senha)";
+
+                MySqlCommand cmd =
+                    new MySqlCommand(sql, conexao.conectar);
+
+                cmd.Parameters.AddWithValue(
+                    "@nome",
+                    usuario.Nome);
+
+                cmd.Parameters.AddWithValue(
+                    "@serie",
+                    usuario.Serie);
+
+                cmd.Parameters.AddWithValue(
+                    "@telefone",
+                    usuario.Telefone);
+
+                cmd.Parameters.AddWithValue(
+                    "@email",
+                    usuario.Email);
+
+                cmd.Parameters.AddWithValue(
+                    "@senha",
+                    usuario.Senha);
+
+                cmd.ExecuteNonQuery();
+
+                conexao.FecharConexao();
+            }
+
+            return RedirectToAction("Login");
         }
 
         public ActionResult Dashboard()
